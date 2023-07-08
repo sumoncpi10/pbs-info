26fe6e9c08c6fca36effe56f397ecc396a3b1a60
Sumon <sumoncpi10@gmail.com>
2023-07-08 15:21:19 +0600

    ok

--- src/Pages/Import/BillImport.js
+++ src/Pages/Import/BillImport.js
@@ -2,33 +2,33 @@ import React, { useState } from 'react';
 import Papa from 'papaparse';
 import { toast } from 'react-toastify';
 const BillImport = () => {
-   const [idata,setData]=useState([])
-   const [coulmn,setCoulmn]=useState([])
-   const [values,setValues]=useState([])
-console.log(idata)
-console.log(coulmn)
-console.log(values)
-   const handleFile=(event)=>{
-    // eslint-disable-next-line no-sequences
-    Papa.parse(event.target.files[0],{
-        header:true,
-        skipEmptyLines:true,
-        complete:function(result){
-            const coulmnArray=[]
-            const valuesArray=[];
-            result.data.map((d)=>{
-                coulmnArray.push(Object.keys(d));
-                valuesArray.push(Object.values(d))
-            })
-            setData(result.data);
-            setCoulmn(coulmnArray[0])
-            setValues(valuesArray)
-        }
-    })
-   }
-   const handleImportBill=(e)=>{
-    e.preventDefault();
-    fetch('https://pbsofficeinfosql.onrender.com/import-csv', {
+    const [idata, setData] = useState([])
+    const [coulmn, setCoulmn] = useState([])
+    const [values, setValues] = useState([])
+    console.log(idata)
+    console.log(coulmn)
+    console.log(values)
+    const handleFile = (event) => {
+        // eslint-disable-next-line no-sequences
+        Papa.parse(event.target.files[0], {
+            header: true,
+            skipEmptyLines: true,
+            complete: function (result) {
+                const coulmnArray = []
+                const valuesArray = [];
+                result.data.map((d) => {
+                    coulmnArray.push(Object.keys(d));
+                    valuesArray.push(Object.values(d))
+                })
+                setData(result.data);
+                setCoulmn(coulmnArray[0])
+                setValues(valuesArray)
+            }
+        })
+    }
+    const handleImportBill = (e) => {
+        e.preventDefault();
+        fetch('https://pbsofficeinfosql.onrender.com/import-csv', {
             method: 'POST',
             // limits:{fileSize: '1gb'},
             headers: {
@@ -41,44 +41,44 @@ console.log(values)
                 console.log('success', data);
                 e.target.reset();
                 // setData("");
-                toast("Data Import Successfully!");
+                toast("Bill Import Successfully!");
             })
-   }
+    }
     return (
         <div className='container '>
             <div className='d-flex flex-column justify-content-center align-items-center'>
                 <h1 className='m-3'>Upload Sales/Bills Here</h1>
                 <form onSubmit={handleImportBill}>
-                    <input 
-                    className='btn btn-light'
-                        type='file' 
-                        name='file' 
-                        accept='.csv' 
-                        onChange={handleFile} 
-                        style={{display:"block" ,margin:"10px auto"}}>
-                        
+                    <input
+                        className='btn btn-light'
+                        type='file'
+                        name='file'
+                        accept='.csv'
+                        onChange={handleFile}
+                        style={{ display: "block", margin: "10px auto" }}>
+
                     </input>
-                    <input type='submit' className='btn btn-primary' value='Import Bill'></input> 
+                    <input type='submit' className='btn btn-primary' value='Import Bill'></input>
                 </form>
-                <br/>
+                <br />
                 <table className="table">
                     <thead>
                         <tr>
                             {
-                                coulmn?.map((col)=><th scope="col">{col}</th>)
+                                coulmn?.map((col) => <th scope="col">{col}</th>)
                             }
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            values?.map(v=>
-                                <tr > {v.map(vv=><th >{vv}</th>)}</tr>
+                            values?.map(v =>
+                                <tr > {v.map(vv => <th >{vv}</th>)}</tr>
                             )
                         }
                     </tbody>
                 </table>
             </div>
-       
+
         </div>
     );
 };
